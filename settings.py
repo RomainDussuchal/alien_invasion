25dde926d2be26fb0bb710fbfe4e56aa380bfdd6
RomainDussuchal <romain-vrb@hotmail.fr>
2024-09-26 17:48:11 +0200

    adding bullets

---
+++ settings.py
@@ -0,0 +1,16 @@
+class Settings:
+    def __init__(self):
+
+        #Screen size settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
+
+        # Ship speed setting
+        self.ship_speed = 3
+
+        # Bullet settings
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
