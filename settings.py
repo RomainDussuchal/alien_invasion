8910f7e8f25bc7621279cf3a622d4710e3454d65
RomainDussuchal <romain-vrb@hotmail.fr>
2024-09-27 17:12:56 +0200

    Killing aliens, and loosing lives

--- settings.py
+++ settings.py
@@ -8,6 +8,7 @@ class Settings:
 
         # Ship speed setting
         self.ship_speed = 3
+        self.ship_limit = 3
 
         # Bullet settings
         self.bullet_speed = 6.0
@@ -15,3 +16,10 @@ class Settings:
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 6
+
+        # Alien settings
+        self.alien_speed = 1.0
+        self.fleet_drop_speed = 10.0
+        self.fleet_direction = 1
+
+
